dcec8a9f67ae52a20991b63109646775a91df0fc
ZainaDali <dalizaina19092000@gmail.com>
2025-11-24 10:30:50 +0100

    struct Link & click

--- internal/models/link.go
+++ internal/models/link.go
@@ -7,3 +7,12 @@ package models
 // Shortcode : doit être unique, indexé pour des recherches rapide (voir doc), taille max 10 caractères
 // LongURL : doit pas être null
 // CreateAt : Horodatage de la créatino du lien
+
+
+// Link représente un lien raccourci dans la base de données.
+type Link struct {
+	ID        uint      `gorm:"primaryKey"`                    // Clé primaire
+	ShortCode string    `gorm:"size:10;uniqueIndex;not null"`  // Code court unique, indexé et max 10 caractères
+	LongURL   string    `gorm:"not null"`                      // Lien original obligatoire
+	CreatedAt time.Time                                       // Horodatage de la création du lien
+}
