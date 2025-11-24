c35fc52a00eaeda9ff462863c9125cba5066d369
ZainaDali <dalizaina19092000@gmail.com>
2025-11-24 12:00:45 +0100

    main

--- main.go
+++ main.go
@@ -1,10 +1,15 @@
 package main
 
 import (
-	_ "github.com/axellelanca/urlshortener/cmd/cli"    // Importe le package 'cli' pour que ses init() soient exécutés
-	_ "github.com/axellelanca/urlshortener/cmd/server" // Importe le package 'server' pour que ses init() soient exécutés
+    "os"
+    
+    "github.com/axellelanca/urlshortener/cmd"
+    _ "github.com/axellelanca/urlshortener/cmd/cli"
+    _ "github.com/axellelanca/urlshortener/cmd/server"
 )
 
 func main() {
-	// TODO
+    if err := cmd.Execute(); err != nil {
+        os.Exit(1)
+    }
 }
